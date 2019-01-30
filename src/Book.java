d0a4ba7c2a4f994e13eb2c3fc9c08d5737559472
Andreas <andreas.oie@hotmail.com>
2019-01-30 11:38:57 +0100

    Created getters and setters in class Book

--- src/Book.java
+++ src/Book.java
@@ -8,17 +8,41 @@ public class Book{
     private int totalEditions;
 
 
-
     public Book(String author, String published, int totalEditions){
         this.author = author;
         this.published = published;
         this.totalEditions = totalEditions;
     }
 
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
+    public String getAuthor() {
+        return author;
+    }
 
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 
+    public String getPublished() {
+        return published;
+    }
 
+    public void setPublished(String published) {
+        this.published = published;
+    }
 
+    public int getTotalEditions() {
+        return totalEditions;
+    }
+
+    public void setTotalEditions(int totalEditions) {
+        this.totalEditions = totalEditions;
+    }
 }
