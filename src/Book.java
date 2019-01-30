de78c1b5a58ce4d94eb7d87caa675fd7eb0bcee6
Andreas <andreas.oie@hotmail.com>
2019-01-30 11:33:36 +0100

    Created book class

--- src/Book.java
+++ src/Book.java
@@ -2,7 +2,23 @@
 
 public class Book{
 
+    private String title;
     private String author;
-    private
+    private String published;
+    private int totalEditions;
+
+
+
+    public Book(String author, String published, int totalEditions){
+        this.author = author;
+        this.published = published;
+        this.totalEditions = totalEditions;
+    }
+
+
+
+
+
+
 
 }
