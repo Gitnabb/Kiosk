4228a9c3b782de9292f402c3eb192e0fe32de157
kjetilhammerseth <kjetilhammerseth@gmail.com>
2019-02-11 11:01:26 +0100

    Added javadoc to class Book.

--- src/Book.java
+++ src/Book.java
@@ -1,20 +1,28 @@
 
 /*
 *
-*
-*
-*
+* Class Book represents a book. Holds information about the books in the Kiosk.
 *
 *
 */
 
 public class Book{
 
-    private String title;
-    private String author;
-    private int published;
-    private int edition;
+    // Necessary fields defined
+    private String title; // Title of a book
+    private String author; // Author of a book
+    private int published; // The year the book was published
+    private int edition; // Which edition it is
 
+    /**
+     *
+     * Creates a book, initialized with the title, author, published and the edition
+     *
+     * @param title     Title of the book
+     * @param author    Author of the book
+     * @param published Year published
+     * @param edition   Edition number
+     */
 
     public Book(String title, String author, int published, int edition){
         this.title = title;
@@ -23,38 +31,81 @@ public class Book{
         this.edition = edition;
     }
 
+    /**
+     * Returns the title of the book when called.
+     * @return the title of the book
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets title with input title
+     * @param title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
+
+    /**
+     * Returns the author of the book when called.
+     * @return the author of the book
+     */
 
     public String getAuthor() {
         return author;
     }
 
+    /**
+     * Sets author with input author
+     * @param author
+     */
     public void setAuthor(String author) {
         this.author = author;
     }
+
+    /**
+     * Returns the publish year of the book when called.
+     * @return the publish year of the book
+     */
 
     public int getPublished() {
         return published;
     }
 
+    /**
+     * Sets the published year with input published
+     * @param published
+     */
+
     public void setPublished(int published) {
         this.published = published;
     }
+
+    /**
+     * Returns the edition of the book when called.
+     * @return the edition of the book
+     */
 
     public int getEdition() {
         return edition;
     }
 
+    /**
+     * Sets edition with input edition
+     * @param edition
+     */
+
     public void setEdition(int edition) {
         this.edition = edition;
     }
 
+
+    /**
+     * Consolidates get methods and returns formatted String.
+     * @Override Skip existing toString method.
+     * @return String
+     */
     @Override
     public String toString() {
         return ("Title: "+this.getTitle()+ ", " +
